966ac124ed76663d29c0a53b203480c66616fcfb
Luke Hackett <Luke.Hackett@sky.uk>
2025-01-03 16:59:07 +0000

    Initial version of the plugin

--- src/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, it } from 'vitest';
-
-it('should be ok', async () => {
-  expect(true).toBeTruthy();
-});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export type { BuildInfoFilePluginConfig, ContributorConfig, GitContributorConfig, Contributor } from './types';
+
+// eslint-disable-next-line import/no-default-export
+export { buildInfoFile as default } from './plugin';
